e4b2d80db01db01bc942c3f9a962a334b935a3fb
jhpark <jhpark@daou.co.kr>
2024-06-26 17:31:34 +0900

    initial commit

---
+++ src/test/java/com/daou/sabangnetserver/SabangnetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daou.sabangnetserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabangnetServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
