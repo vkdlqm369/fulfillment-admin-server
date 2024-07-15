d24adba90f9d1b3c46ea4a1352ff7365288e035d
nirey929 <nirey929@gmail.com>
2024-07-15 14:44:49 +0900

    fix: 사용하지 않는 setter 제거 및 라우터 상수 리스트 변경

--- src/main/java/com/daou/sabangnetserver/domain/user/entity/Authority.java
+++ src/main/java/com/daou/sabangnetserver/domain/user/entity/Authority.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table (name = "AUTHORITY")
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
