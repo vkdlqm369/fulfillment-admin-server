f21f247b0b46ee34bf63e1bdfdffd8e230f167f6
홍진수 <h1101201@daou.co.kr>
2024-07-23 08:19:44 +0900

    fix : 주문수집 반환 형식 수정

--- src/main/java/com/daou/sabangnetserver/controller/OrderCollectController.java
+++ src/main/java/com/daou/sabangnetserver/controller/OrderCollectController.java
@@ -10,6 +10,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -22,7 +24,10 @@ public class OrderCollectController {
     @GetMapping("/order/{sellerNo}")
     public ResponseEntity<String> orderCollectData(@ModelAttribute OrderRequestDto orderRequestDto) {
         orderCollectService.fetchAndSaveOrders(orderRequestDto);
-        return ResponseEntity.ok("Orders fetched and saved successfully.");
+        LocalDate start = LocalDate.parse(orderRequestDto.getStartDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate end = LocalDate.parse(orderRequestDto.getEndDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String message = String.format("선택하신 날짜 %s부터 %s까지 주문 수집이 완료되었습니다.", start, end);
+        return ResponseEntity.ok(message);
     }
 
 }
