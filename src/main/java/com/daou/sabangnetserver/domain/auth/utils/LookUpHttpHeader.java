74a1baace5955967406431d418fc6bc13a840e2f
nirey929 <nirey929@gmail.com>
2024-08-08 13:03:23 +0900

    fix: IP 에러 해결

--- src/main/java/com/daou/sabangnetserver/domain/auth/utils/LookUpHttpHeader.java
+++ src/main/java/com/daou/sabangnetserver/domain/auth/utils/LookUpHttpHeader.java
@@ -12,6 +12,10 @@ public class LookUpHttpHeader {
 
         String ip = request.getHeader("X-Forwarded-For");
 
+        if(ip != null && !ip.isEmpty()){
+            ip = ip.split(",")[0].trim();
+        }
+
         if (ip == null)
             ip = request.getHeader("Proxy-Client-IP");
         if (ip == null)
